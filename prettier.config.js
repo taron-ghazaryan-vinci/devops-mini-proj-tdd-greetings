d6edce84d473de55857ad7a5f18cf4752611d5c2
ghazaryan.taron <taron.ghazaryan@student.vinci.be>
2023-12-13 10:59:27 +0100

    initial commit

--- prettier.config.js
+++ prettier.config.js
@@ -2,10 +2,10 @@
 
 /** @type {import("prettier").Config} */
 const config = {
-    trailingComma: "es5",
+    trailingComma: 'es5',
     tabWidth: 4,
     semi: false,
     singleQuote: true,
-  };
-  
-  module.exports = config;
+}
+
+module.exports = config
