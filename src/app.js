2ad570495438d82126a0ed0c2064ba51c53771a0
ghazaryan.taron <taron.ghazaryan@student.vinci.be>
2023-12-13 16:39:37 +0100

    Fix: Linter and Prettier test

--- src/app.js
+++ src/app.js
@@ -1,66 +1,66 @@
-function greet(name) {
-    let hello = 'Hello'
-    let and = ' and '
-    let language = 'en'
-    if (!Array.isArray(name)) {
-        return isNotArray(name)
-    } else {
-        language = findLanguage(name, language)
-        if (language === 'fr') {
-            hello = 'Bonjour'
-            and = ' et '
-        } else if (language === 'nl') {
-            hello = 'Goeidag'
-            and = ' en '
-        }
-        const nameUpperCase = []
-        for (let i = 0; i < name.length; i++) {
-            if (name[i] === name[i].toUpperCase()) {
-                nameUpperCase.push(name[i])
-                name.splice(i, 1)
-                i--
-            }
-        }
-        if (name.length === 0) {
-            return onlyUpperCaseNames(nameUpperCase, hello, and)
-        }
-        return mixedCaseNames(nameUpperCase, name, hello, and)
+function greet (name) {
+  let hello = 'Hello'
+  let and = ' and '
+  let language = 'en'
+  if (!Array.isArray(name)) {
+    return isNotArray(name)
+  } else {
+    language = findLanguage(name, language)
+    if (language === 'fr') {
+      hello = 'Bonjour'
+      and = ' et '
+    } else if (language === 'nl') {
+      hello = 'Goeidag'
+      and = ' en '
     }
-
-    function isNotArray(name) {
-        if (!name || name.trim().length === 0) {
-            return 'Hello, my friend'
-        }
-        if (name.toUpperCase() === name) {
-            return 'HELLO, ' + name.toUpperCase() + '!'
-        }
-        return 'Hello, ' + name
+    const nameUpperCase = []
+    for (let i = 0; i < name.length; i++) {
+      if (name[i] === name[i].toUpperCase()) {
+        nameUpperCase.push(name[i])
+        name.splice(i, 1)
+        i--
+      }
     }
+    if (name.length === 0) {
+      return onlyUpperCaseNames(nameUpperCase, hello, and)
+    }
+    return mixedCaseNames(nameUpperCase, name, hello, and)
+  }
 
-    function onlyUpperCaseNames(nameUpperCase, hello, and) {
-        if (nameUpperCase.length > 2) {
-            const lastElementUpperCase = nameUpperCase.pop()
-            return (
-                hello.toUpperCase() +
+  function isNotArray (name) {
+    if (!name || name.trim().length === 0) {
+      return 'Hello, my friend'
+    }
+    if (name.toUpperCase() === name) {
+      return 'HELLO, ' + name.toUpperCase() + '!'
+    }
+    return 'Hello, ' + name
+  }
+
+  function onlyUpperCaseNames (nameUpperCase, hello, and) {
+    if (nameUpperCase.length > 2) {
+      const lastElementUpperCase = nameUpperCase.pop()
+      return (
+        hello.toUpperCase() +
                 ', ' +
                 nameUpperCase.join(', ') +
                 and.toUpperCase() +
                 lastElementUpperCase +
                 '!'
-            )
-        } else {
-            return (
-                hello.toUpperCase() + ', ' + nameUpperCase.join(' AND ') + '!'
-            )
-        }
+      )
+    } else {
+      return (
+        hello.toUpperCase() + ', ' + nameUpperCase.join(' AND ') + '!'
+      )
     }
+  }
 
-    function mixedCaseNames(nameUpperCase, name, hello, and) {
-        const lastElement = name.pop()
-        if (nameUpperCase.length > 1) {
-            const lastElementUpperCase = nameUpperCase.pop()
-            return (
-                hello +
+  function mixedCaseNames (nameUpperCase, name, hello, and) {
+    const lastElement = name.pop()
+    if (nameUpperCase.length > 1) {
+      const lastElementUpperCase = nameUpperCase.pop()
+      return (
+        hello +
                 ', ' +
                 name.join(', ') +
                 and +
@@ -73,11 +73,11 @@ function greet(name) {
                 ' AND ' +
                 lastElementUpperCase +
                 '!'
-            )
-        }
-        if (nameUpperCase.length === 1) {
-            return (
-                hello +
+      )
+    }
+    if (nameUpperCase.length === 1) {
+      return (
+        hello +
                 ', ' +
                 name.join(', ') +
                 and +
@@ -88,22 +88,22 @@ function greet(name) {
                 ' ' +
                 nameUpperCase[0] +
                 '!'
-            )
-        } else {
-            return hello + ', ' + name.join(', ') + and + lastElement + '.'
-        }
+      )
+    } else {
+      return hello + ', ' + name.join(', ') + and + lastElement + '.'
     }
+  }
 
-    function findLanguage(name, language) {
-        for (let i = 0; i < name.length; i++) {
-            if (name[i] === 'fr' || name[i] === 'nl' || name[i] === 'en') {
-                language = name[i]
-                name.splice(i, 1)
-                break
-            }
-        }
-        return language
+  function findLanguage (name, language) {
+    for (let i = 0; i < name.length; i++) {
+      if (name[i] === 'fr' || name[i] === 'nl' || name[i] === 'en') {
+        language = name[i]
+        name.splice(i, 1)
+        break
+      }
     }
+    return language
+  }
 }
 
 module.exports = greet
